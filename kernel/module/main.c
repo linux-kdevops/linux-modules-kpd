c598566d4d356b6bde32d2d07b39683b350cd12a
Richard Guy Briggs <rgb@redhat.com>
2024-10-23 17:13:20 -0400

    audit,module: restore audit logging in load failure case
    The move of the module sanity check to earlier skipped the audit logging
    call in the case of failure and to a place where the previously used
    context is unavailable.
    
    Add an audit logging call for the module loading failure case and get
    the module name when possible.
    
    Link: https://issues.redhat.com/browse/RHEL-52839
    Fixes: 02da2cbab452 ("module: move check_modinfo() early to early_mod_check()")
    Signed-off-by: Richard Guy Briggs <rgb@redhat.com>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -3332,8 +3332,10 @@ static int load_module(struct load_info *info, const char __user *uargs,
 	 * failures once the proper module was allocated and
 	 * before that.
 	 */
-	if (!module_allocated)
+	if (!module_allocated) {
+		audit_log_kern_module(info->name ? info->name : "(unavailable)");
 		mod_stat_bump_becoming(info, flags);
+	}
 	free_copy(info, flags);
 	return err;
 }
