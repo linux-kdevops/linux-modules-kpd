ef94950d5844cd7ec9041fcbd1425588e3f99b99
Daniel Gomez <da.gomez@samsung.com>
2024-11-08 17:12:14 +0100

    module: avoid double ref drop on ro_after_init failure
    Ensure the module reference is only dropped once in case we fail at
    MOD_RO_AFTER_INIT case.
    
    Signed-off-by: Daniel Gomez <da.gomez@samsung.com>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -2941,8 +2941,6 @@ static noinline int do_init_module(struct module *mod)
 	ftrace_free_mem(mod, mod->mem[MOD_INIT_TEXT].base,
 			mod->mem[MOD_INIT_TEXT].base + mod->mem[MOD_INIT_TEXT].size);
 	mutex_lock(&module_mutex);
-	/* Drop initial reference. */
-	module_put(mod);
 	trim_init_extable(mod);
 #ifdef CONFIG_KALLSYMS
 	/* Switch to core kallsyms now init is done: kallsyms may be walking! */
@@ -2951,6 +2949,8 @@ static noinline int do_init_module(struct module *mod)
 	ret = module_enable_rodata_ro(mod, true);
 	if (ret)
 		goto fail_mutex_unlock;
+	/* Drop initial reference. */
+	module_put(mod);
 	mod_tree_remove_init(mod);
 	module_arch_freeing_init(mod);
 	for_class_mod_mem_type(type, init) {
